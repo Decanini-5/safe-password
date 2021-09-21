93caa274de27296c647f7441e8d57aa95e8a8f6f
Marvin Parada <hello@marvin.dev>
2021-09-20 17:38:43 -0700

    SP-2 | Refactor password algorithm.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,24 @@
 <template>
-  <div class="home">This is the Home view.</div>
+  <div class="home">
+    <h1>Safe Password</h1>
+    <div>Press button to generate password.</div>
+    <button @click="generatePassword">Generate Password</button>
+    <div>My password is: {{ password }}</div>
+  </div>
 </template>
 
 <script>
+import generatePasswordFromFile from '@/utilities/password';
+
 export default {
   name: 'Home',
+  data: () => ({
+    password: null,
+  }),
+  methods: {
+    generatePassword() {
+      this.password = generatePasswordFromFile();
+    },
+  },
 };
 </script>
