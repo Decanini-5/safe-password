057379037e57476f2543658a35746246f6dc2e9b
Fernando Decanini <fernando.decanini@gmail.com>
2021-10-28 13:38:13 -0500

    Definition of page layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,10 @@ module.exports = {
     '@vue/airbnb',
   
   ],
-  plugins: ['prettier'],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+//  plugins: ['prettier'],
+//  parserOptions: {
+//    parser: 'babel-eslint',
+//  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <h1>Safe Password</h1>
-    <div>Press button to generate password.</div>
-    <button @click="generatePassword">Generate Password</button>
-    <div>My password is: {{ password }}</div>
+    <img src="..\safe-password-logo.png" alt="sp_logo">
+    <div class="text">Press button to generate password.</div>
+    <button class="btn" @click="generatePassword">GENERATE PASSWORD</button>
+    <div class="text">My password is:</div>
+    <div class="text" id="pswd">{{ password }}</div>
   </div>
 </template>
 
@@ -22,3 +23,55 @@ export default {
   },
 };
 </script>
+<style>
+body {
+  background-color: rgb(25, 25, 25);
+}
+.home {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  background: rgb(26, 50, 95);
+  border-radius: 8px;
+  border: 0;
+  height: 34.8vw;
+  min-height: 430px;
+  width: 23vw;
+  min-width: 285px;
+}
+.btn {
+  background: rgb(10, 10, 10);
+  color: #fff;
+  width: 65%;
+  border-radius: 5px;
+  padding: 0.7rem;
+  border: none;
+  margin-top: 7%;
+  margin-bottom: 10%;
+  font-family: Montserrat, sans-serif;
+  font-size: calc(12px + 0.1vw);
+  cursor: pointer;
+}
+.text {
+  font-family: 'Work Sans', sans-serif;
+  font-size: calc(13px + 0.11vw);
+  padding: 0 5%;
+  color: rgb(178, 184, 185);
+  margin-left: 5px;
+  cursor: default;
+}
+img {
+  width: 60%;
+  margin: 10% 0 20% 0;
+}
+#pswd {
+  font-family: Montserrat, sans-serif;
+  font-weight: bold;
+  font-size: 120%;
+  margin-top: 5%;
+  margin-bottom: 2%;
+  cursor: text;
+}
+</style>
